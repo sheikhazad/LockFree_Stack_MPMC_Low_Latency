bfc3f5789c66e9ee5ea9422ee86637c9e907f726
MD AZAD ALI SHAH <46038479+sheikhazad@users.noreply.github.com>
2025-06-09 23:53:54 +0100

    comment added

--- LockFreeStack.cpp
+++ LockFreeStack.cpp
@@ -84,7 +84,7 @@ int main() {
         for (int i = 0; i < 10; ++i) {
             stack.push(i);  // Simulate order placement
             std::cout << "Produced: " << i << std::endl;
-            std::this_thread::sleep_for(std::chrono::milliseconds(10));  // Simulate latency
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));  // Simulate latency to mimic network/processing latency.
         }
     };
 
