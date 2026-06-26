8709defd638d839283caf12d7ea3f0eb04e98cb7
MD AZAD ALI SHAH <46038479+sheikhazad@users.noreply.github.com>
2026-06-26 17:30:48 +0100

    Add hardware interference size and constants

--- Constants.hpp
+++ Constants.hpp
@@ -1,1 +1,16 @@
+#pragma once
+#include <new> // std::hardware_destructive_interference_size
 
+#ifdef __cpp_lib_hardware_interference_size
+    using std::hardware_destructive_interference_size;
+#else
+    static constexpr size_t hardware_destructive_interference_size = 64;
+#endif
+
+constexpr int NUM_PRODUCERS = 4;
+constexpr int NUM_CONSUMERS = 4;
+constexpr int WORKLOAD = 1000;
+constexpr int NUMA_NODE_0 = 0;
+constexpr int NUMA_NODE_1 = 1;
+
+constexpr size_t CACHE_LINE_SIZE = hardware_destructive_interference_size;
