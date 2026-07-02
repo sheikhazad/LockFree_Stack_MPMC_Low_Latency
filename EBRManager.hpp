9ec688ea0527002c6a2b09b6f61412386aab3977
MD AZAD ALI SHAH <46038479+sheikhazad@users.noreply.github.com>
2026-07-02 06:31:14 +0100

    Update EBRManager.hpp

--- EBRManager.hpp
+++ EBRManager.hpp
@@ -29,11 +29,13 @@ Reclaimer does:
 class EBRManager
 {
 private:
+    //int enough for max thread 128
     static constexpr int MAX_THREADS = 128;
+    std::atomic<int> next_tid{0};
+
+    //uint64_t for epoch 
     //How long we delay reclamation (helps avoid race edge cases)
     static constexpr uint64_t RETIRE_DELAY = 2;
-
-    std::atomic<int> next_tid{0};
     //Global epoch (advanced during reclamation)
     std::atomic<uint64_t> global_epoch{0};
 
